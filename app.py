302e884af8c24d6b7a50dfa4494397fb18f89eae
AryaShukla-coder <arya.bnsd@gmail.com>
2025-12-22 22:44:36 +0530

    Implemented authentication logic and user data handling using JSON storage

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from db import Database
 
 app = Flask(__name__)
+dbo = Database()
 
 @app.route('/')
 def index():
@@ -9,5 +11,35 @@ def index():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/perform_registration',methods=['post'])
+def perform_registration():
+    fname = request.form.get('user_fname')
+    lname = request.form.get('user_lname')
+    email = request.form.get('user_email')
+    password = request.form.get('user_password')
+
+    response = dbo.insert(fname,lname,email,password)
+
+    if response:
+        return render_template("login.html", message="Registration successful. Kindly login to proceed")
+    else:
+        return render_template("register.html", message="Email already exists")
+
+    return fname + " " + lname + " " + email + " " + password
+
+@app.route('/perform_login',methods=['post'])
+def perform_login():
+    email = request.form.get('user_email')
+    password = request.form.get('user_password')
+
+    response = dbo.search(email, password)
+
+    if response:
+        return "Welcome"
+    else:
+        return render_template("login.html", message="Incorrect email/password")
+
+
 
 app.run(debug=True)
